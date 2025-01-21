e0eb92f9a01411e8cb299b84b27569300528c6eb
spaceishere <batocirdaramjav@gmail.com>
2025-01-22 02:49:12 +0800

    dome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
+
 import Image from "next/image";
+import loveImage from "/public/love.png";
+import cute from "/public/cute.gif";
+import yes from "/public/yes.gif";
+import bat from "/public/bat.gif";
+import { useState } from "react";
+import Confetti from "react-confetti";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [serius, setSerius] = useState(false);
+  const [app, setApp] = useState(false);
+  const [notapp, SetNotApp] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-[url('https://img.freepik.com/free-vector/doodle-background-star-pattern-ink-design-vector_53876-154207.jpg')] bg-center">
+      {app && (
+        <div className="w-full fixed z-20 left-0 top-0">
+          <Confetti />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      )}
+      <div
+        className={` ${
+          serius || app || notapp ? "hidden" : "flex"
+        } w-[80%] sm:w-[50%] lg:w-[350px] text-center flex flex-col gap-[20px] items-center justify-center bg-white rounded-xl shadow-2xl p-[50px]`}
+      >
+        <p className="text-[25px] text-center">Dedicated to ezu</p>
+        <Image src={loveImage} alt="love" width={350} height={150} />
+        <p className="text-[20px] text-center">Батукатай болзохуу?</p>
+        <div className="w-[100%] flex items-center justify-center gap-4">
+          <button
+            className="border-[1px] shadow-md px-4 py-2 border-black rounded-md"
+            onClick={() => {
+              setApp(true);
+              setSerius(false);
+            }}
+          >
+            Тийм{" "}
+          </button>
+          <button
+            className="border-[1px] shadow-md px-4 py-2 border-black rounded-md group relative"
+            onClick={() => {
+              setSerius(true);
+            }}
+          >
+            <span className="group-hover:hidden">Үгүй</span>
+            <span className="hidden group-hover:inline">Үнэхээр итгэлтэй байна уу ?</span>
+          </button>
+        </div>
+      </div>
+      <div className={`${serius ? "flex" : "hidden"}  w-[100%]   flex items-center justify-center`}>
+        <div className="w-[80%] sm:w-[50%] lg:w-[350px] p-[50px] shadow-2xl bg-white rounded-lg flex flex-col gap-2 items-center justify-center text-black">
+          <Image src={cute} alt="love" width={350} height={150} className="rounded-lg" />
+          <p className="text-[20px] text-center">
+            Үнэхээр итгэлтэй байна уу ? <br />
+            Дахиад нэг бодоод үзэх юмуу?
+          </p>
+          <div className="flex gap-2">
+            <button
+              className=" shadow-md px-4 py-2 border-black rounded-md bg-white border-[1px]  text-black "
+              onClick={() => {
+                setApp(true);
+                setSerius(false);
+              }}
+            >
+              Зза тэгье даа{" "}
+            </button>
+            <button
+              className=" shadow-md px-4 py-2 border-black rounded-md bg-white border-[1px]  text-black"
+              onClick={() => {
+                SetNotApp(true);
+                setSerius(false);
+              }}
+            >
+              Үгүй ээ үгүй{" "}
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={`${app ? "flex" : "hidden"}  w-full h-screen flex items-center justify-center`}>
+        <div className="w-[80%] sm:w-[50%] lg:w-[350px] p-[50px] shadow-2xl bg-white rounded-lg flex flex-col gap-2 items-center justify-center text-black">
+          <Image src={yes} alt="love" width={350} height={150} className="rounded-lg" />
+          <p className="text-[20px] text-center">Надад боломж олгож зөвшөөрсөнд баярлалаа.</p>
+        </div>
+      </div>
+      <div className={`${notapp ? "flex" : "hidden"}  w-full h-screen flex items-center justify-center`}>
+        <div className="w-[80%] sm:w-[50%] lg:w-[350px] p-[50px] shadow-2xl bg-white rounded-lg flex flex-col gap-2 items-center justify-center text-black">
+          <Image src={bat} alt="love" width={350} height={150} className="rounded-lg" />
+          <p className="text-[20px] text-center">Би заавал буцаж ирнээ амлаж байна.</p>
+        </div>
+      </div>
     </div>
   );
 }
